764add67c71bee8437a29af3880ff16c91f2fec1
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-01-08 23:55:23 +0900

    setup sminimal kernel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // disable the standard library
+#![no_main] // disable all Rust-level entry points
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // this function is the entrypoint, since the linker looks for a function
+    // named `_start` by default
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
