bc0c734667be6f12d4aaab0eef652c5fdf5b2bbb
dugi <dugi@channel.io>
2023-01-12 09:50:32 +0900

    test: write test outputs automatically

--- src/main.rs
+++ src/main.rs
@@ -54,17 +54,30 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+    where
+        T: Fn(),
+{
+    fn run(&self) -> () {
+        serial_print!("{} ...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("it is not broken... ");
-    assert_eq!(0, 1);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
 }
