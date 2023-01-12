6a65c1983c98bb1a4f35628473efd18754ecd558
dugi <dugi@channel.io>
2023-01-12 10:14:09 +0900

    refactor: separate lib module for integrated test

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+#![no_std] // disable the standard library
+#![cfg_attr(test, no_main)]
+#![warn(clippy::all, clippy::pedantic)] // enable clippy lints
+#![feature(custom_test_frameworks)] // enable testing with #[no_std] context
+#![test_runner(crate::test_runner)] // define custom test framework runner
+#![reexport_test_harness_main = "test_main"] // rename the test entry function to `test_main`
+
+pub mod serial;
+pub mod vga_buffer;
+
+mod qemu;
+mod test;
+
+pub fn test_runner(tests: &[&dyn test::Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    qemu::exit(qemu::ExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &core::panic::PanicInfo) -> ! {
+  serial_println!("[failed]\n");
+  serial_println!("Error: {info}\n");
+  qemu::exit(qemu::ExitCode::Failed);
+}
+
+/// Entry point for `cargo test`
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    test_panic_handler(info)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,8 @@
 #![no_main] // disable all Rust-level entry points
 #![warn(clippy::all, clippy::pedantic)] // enable clippy lints
 #![feature(custom_test_frameworks)] // enable testing with #[no_std] context
-#![test_runner(crate::test_runner)] // define custom test framework runner
+#![test_runner(rust_os::test_runner)] // define custom test framework runner
 #![reexport_test_harness_main = "test_main"] // rename the test entry function to `test_main`
-
-mod serial;
-mod qemu;
-mod test;
-mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -26,7 +21,7 @@ pub extern "C" fn _start() -> ! {
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-    println!("{info}");
+    rust_os::println!("{info}");
     
     #[allow(clippy::empty_loop)]
     loop {}
@@ -36,16 +31,5 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {info}\n");
-    qemu::exit(qemu::ExitCode::Failed);
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn test::Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    qemu::exit(qemu::ExitCode::Success);
+    rust_os::test_panic_handler(info);
 }
