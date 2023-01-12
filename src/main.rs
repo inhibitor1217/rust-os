401f83487d6a13f9c1336168f0ac0ddf10f3837d
dugi <dugi@channel.io>
2023-01-12 09:42:53 +0900

    test: write test outputs through serial port, forwarding to console

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 #![test_runner(crate::test_runner)] // define custom test framework runner
 #![reexport_test_harness_main = "test_main"] // rename the test entry function to `test_main`
 
+mod serial;
 mod vga_buffer;
 
 #[no_mangle]
@@ -43,7 +44,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
@@ -52,7 +53,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("it is not broken... ");
+    serial_print!("it is not broken... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
---
+++ src/serial.rs
@@ -0,0 +1,31 @@
+use core::fmt;
+use lazy_static::lazy_static;
+use spin::Mutex;
+use uart_16550::SerialPort;
+
+lazy_static! {
+    pub static ref SERIAL1: Mutex<SerialPort> = {
+        let mut serial_port = unsafe { SerialPort::new(0x3F8) };
+        serial_port.init();
+        Mutex::new(serial_port)
+    };
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    SERIAL1.lock().write_fmt(args).unwrap();
+}
+
+/// Prints to the host through the serial interface.
+#[macro_export]
+macro_rules! serial_print {
+  ($($arg:tt)*) => ($crate::serial::_print(format_args!($($arg)*)));
+}
+
+/// Prints to the host through the serial interface, appending a newline.
+#[macro_export]
+macro_rules! serial_println {
+  () => ($crate::serial_print!("\n"));
+  ($($arg:tt)*) => ($crate::serial_print!("{}\n", format_args!($($arg)*)));
+}
