83fdac699a302fc0eb78a5368bdd00ef62b0a33c
dugi <dugi@channel.io>
2023-01-16 08:29:21 +0900

    feat: handle timer interrupts

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -3,7 +3,18 @@ use pic8259::ChainedPics;
 use spin::Mutex;
 use x86_64::{structures::idt::{InterruptDescriptorTable, InterruptStackFrame}, instructions};
 
-use crate::{gdt, println};
+use crate::{gdt, println, print};
+
+const PIC_1_OFFSET: u8 = 0x20; // Primary Interrupt Controller: Interrupt vectors from 0x20 to 0x27
+const PIC_2_OFFSET: u8 = 0x28; // Secondary Interrupt Controller: Interrupt vectors from 0x28 to 0x2f
+
+static PICS: Mutex<ChainedPics> = Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+enum InterruptIndex {
+    Timer = PIC_1_OFFSET, // Line 0 of Primary Interrupt Controller
+}
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -14,6 +25,7 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt[InterruptIndex::Timer as usize].set_handler_fn(timer_interrupt_handler);
         idt
     };
 }
@@ -33,10 +45,12 @@ extern "x86-interrupt" fn double_fault_handler(
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
-const PIC_1_OFFSET: u8 = 0x20; // Primary Interrupt Controller: Interrupt vectors from 0x20 to 0x27
-const PIC_2_OFFSET: u8 = 0x28; // Secondary Interrupt Controller: Interrupt vectors from 0x28 to 0x2f
-
-static PICS: Mutex<ChainedPics> = Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    print!(".");
+    unsafe {
+        PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer as u8);
+    }
+}
 
 pub fn init_pic() {
     unsafe { PICS.lock().initialize(); }
